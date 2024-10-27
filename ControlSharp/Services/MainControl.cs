fefc9f0d1d4a2b9a65580fabbb711d9638bd34c4
Niels Schuler <niels.schuler@web.de>
2024-10-27 13:04:55 +0100

    - Added Method for Creating Init Admin

--- ControlSharp/Services/MainControl.cs
+++ ControlSharp/Services/MainControl.cs
@@ -30,6 +30,40 @@ public class MainControl : IHostedService
     private void StartUp(CancellationToken Token)
     {
         _context.Database.EnsureCreated();
+        if (Created)
+        {
+            _logger.LogInformation("Created Database");
+            Access Admin = new Access()
+            {
+                Id = Guid.NewGuid(),
+                Asset = new Asset()
+                {
+                    Id = Guid.Empty,
+                    Created = DateTimeOffset.Now,
+                    Name = "Admin"
+                },
+                Key = new ApiKey()
+                {
+                    Id = Guid.NewGuid(),
+                    Active = true,
+                    Created = DateTimeOffset.Now,
+                    Key = CreateAdminToken()
+                }
+            };
+            
+            _context.Access.Add(Admin);
+            _context.SaveChanges();
+            
+            _logger.LogInformation("Created Admin");
+            _logger.LogInformation($"UserID: {Admin.Asset.Id}");
+            _logger.LogInformation($"Key: {Admin.Key.Key}");
+            
+        }
+        else
+        {
+            _logger.LogInformation("Database found");
+        }
+    }
     private string CreateAdminToken()
     {
         byte[] TokenData = RandomNumberGenerator.GetBytes(_adminKeySize);
