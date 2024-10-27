db66e9cd3183f831949b3a7d760ebcab3fa8a2d7
Niels Schuler <niels.schuler@web.de>
2024-10-27 10:07:32 +0100

    Renamed and Register DB

--- ControlSharp/Program.cs
+++ ControlSharp/Program.cs
@@ -1,13 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+using ControlSharp.Config;
+using Microsoft.EntityFrameworkCore;
+
+const string DatabaseConnection = "DefaultConnection";
+
+WebApplicationBuilder Builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+Builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+Builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseSqlite(ConnectionData);
+});
+
+var app = Builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
