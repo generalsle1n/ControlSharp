6c36e0ee9920a0306d9cda92c493320904da1027
Niels Schuler <niels.schuler@web.de>
2024-10-27 13:24:09 +0100

    - Updateing Asset on Access - Enriched Data with Include

--- ControlSharp/Filter/ApiAuthFilter.cs
+++ ControlSharp/Filter/ApiAuthFilter.cs
@@ -2,6 +2,7 @@ using ControlSharp.Config;
 using ControlSharp.Config.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Primitives;
 
 namespace ControlSharp.Filter;
@@ -28,11 +29,21 @@ public class ApiAuthFilter : IAsyncAuthorizationFilter
             //Check if HeaderKey is an valid Guid
             if(Result)
             {
-                IQueryable<Access> DatabaseResult = _context.Access.Where(item => item.Asset.Id == AssetID && item.Key.Key == SingleHeader.Value.First());
+                IQueryable<Access> DatabaseResult = _context.Access.Where(item => item.Asset.Id == AssetID && item.Key.Key == SingleHeader.Value.First())
+                    .Include(item => item.Asset);
 
                 if (DatabaseResult.Count() == 1)
                 {
                     LoginFailed = false;
+                    
+                    Asset Current = DatabaseResult.First().Asset;
+                    
+                    Current.LastOnline = DateTimeOffset.Now;
+                    Current.Ip = context.HttpContext.Connection.RemoteIpAddress.ToString();
+                    
+                    _context.Asset.Update(Current);
+                    await _context.SaveChangesAsync();
+                    
                     break;
                 }
             }
