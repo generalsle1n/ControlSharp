28153d35cb75ff482d6d7f4e41a7cf5d0491f086
Niels Schuler <niels.schuler@web.de>
2024-10-27 12:59:59 +0100

    Allowed Nullable Type

--- ControlSharp/Database/Model/Asset.cs
+++ ControlSharp/Database/Model/Asset.cs
@@ -7,6 +7,6 @@ public class Asset
     public required Guid Id { get; set; }
     public required string Name { get; set; }
     public required DateTimeOffset Created { get; set; }
-    public DateTimeOffset LastOnline { get; set; }
-    public IPAddress Ip { get; set; }
+    public DateTimeOffset? LastOnline { get; set; }
+    public IPAddress? Ip { get; set; }
 }
