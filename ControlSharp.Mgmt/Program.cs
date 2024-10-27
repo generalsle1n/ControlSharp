aa6d4744aa7d9cab5c01bd62e57e16f00c7ad9ee
Niels Schuler <niels.schuler@web.de>
2024-10-27 17:42:03 +0100

    Added

--- ControlSharp.Mgmt/Program.cs
+++ ControlSharp.Mgmt/Program.cs
@@ -1,5 +1,11 @@
-var builder = DistributedApplication.CreateBuilder(args);
+using Microsoft.Extensions.Configuration;
+using Projects;
 
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<ControlSharp_Api>("ControlSharp-Api")
+    .WithReplicas(builder.Configuration.GetValue<int>("Replicas"));
+builder.AddProject<ControlSharp_Ui>("ControlSharp-Ui")
+    .WithReplicas(builder.Configuration.GetValue<int>("Replicas"));
 
 builder.Build().Run();
