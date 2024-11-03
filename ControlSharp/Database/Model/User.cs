919d91f36970e2120c362f16fd489e8dc860a0d3
Niels Schuler <niels.schuler@web.de>
2024-11-03 10:30:28 +0100

    Removed not required properties

--- ControlSharp/Database/Model/User.cs
+++ ControlSharp/Database/Model/User.cs
@@ -4,12 +4,6 @@ namespace ControlSharp.Api.Config.Model;
 
 public class User : IdentityUser
 {
-    public required Guid Id { get; set; }
-    public required string UserName { get; set; }
-    public required string Password { get; set; }
-    public required AccessRole Role { get; set; }
-    public bool Active { get; set; }
-    public required DateTimeOffset Created { get; set; }
     public DateTimeOffset? Created { get; set; }
     public DateTimeOffset? LastOnline { get; set; }
 }
