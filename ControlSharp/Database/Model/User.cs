f2fcb3353148db0a81028de9ca2805a6469ef0be
Niels Schuler <niels.schuler@web.de>
2024-11-03 02:36:34 +0100

    Modified for Identity

--- ControlSharp/Database/Model/User.cs
+++ ControlSharp/Database/Model/User.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace ControlSharp.Api.Config.Model;
 
-public class User
+public class User : IdentityUser
 {
     public required Guid Id { get; set; }
     public required string UserName { get; set; }
@@ -8,5 +10,6 @@ public class User
     public required AccessRole Role { get; set; }
     public bool Active { get; set; }
     public required DateTimeOffset Created { get; set; }
+    public DateTimeOffset? Created { get; set; }
     public DateTimeOffset? LastOnline { get; set; }
 }
