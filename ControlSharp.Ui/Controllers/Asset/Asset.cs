dce43da874860857b20902a219c99fda9ddfdd36
Niels Schuler <niels.schuler@web.de>
2024-11-09 18:05:59 +0100

    Added Asset.cs Controller

---
+++ ControlSharp.Ui/Controllers/Asset/Asset.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlSharp.Ui.Controllers.Asset;
+
+public class Asset : Controller
+{
+    // public IActionResult Index()
+    // {
+    //     return View();
+    // }
+}
