068926f1151e699d74d7825d911be24f98fff8f6
Niels Schuler <niels.schuler@web.de>
2024-11-10 20:13:42 +0100

    Asset.cs  - Fixed Typo  - Moved Hash to required

--- ControlSharp.Database/Identity/Model/Asset.cs
+++ ControlSharp.Database/Identity/Model/Asset.cs
@@ -6,10 +6,10 @@ public class Asset
 {
     public required Guid Id { get; set; }
     public required string Name { get; set; }
+    public required string Hash { get; set; }
     public required DateTimeOffset Created { get; set; }
     public DateTimeOffset? LastOnline { get; set; }
     public string? Ip { get; set; }
     public bool? Active { get; set; }
-    public string? Hash { get; set; }
-    public bool? Registerd { get; set; }
+    public bool? Registered { get; set; }
 }
