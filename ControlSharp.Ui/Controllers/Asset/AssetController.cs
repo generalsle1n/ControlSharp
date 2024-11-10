8ac3fcd942c053256d4234fcc45860a629d6bdc0
Niels Schuler <niels.schuler@web.de>
2024-11-10 10:35:59 +0100

    Added DI into Controller

--- ControlSharp.Ui/Controllers/Asset/AssetController.cs
+++ ControlSharp.Ui/Controllers/Asset/AssetController.cs
@@ -6,6 +6,16 @@ namespace ControlSharp.Ui.Controllers.Asset;
 public class AssetController : Controller
 {
     public IActionResult Asset()
+    private readonly ILogger<AssetController> _logger;
+    private IHttpClientFactory _httpClientFactory;
+    private SignInManager<User> _signInManager;
+    public AssetController(ILogger<AssetController> logger, IHttpClientFactory httpClientFactory, SignInManager<User> signInManager)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+        _signInManager = signInManager;
+    }
+    
     {
         return View();
     }
