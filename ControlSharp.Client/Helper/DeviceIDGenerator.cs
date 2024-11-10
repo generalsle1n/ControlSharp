31da882e13fb4e97b46f841237189ed387879190
Niels Schuler <niels.schuler@web.de>
2024-11-10 15:48:40 +0100

    Added ID Generator

---
+++ ControlSharp.Client/Helper/DeviceIDGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ControlSharp.Client.Helper;
+
+public class DeviceIDGenerator
+{
+    public static async Task<string> GenerateAsync()
+    {
+        string hostname = Dns.GetHostName();
+        using (SHA256 Hasher = SHA256.Create())
+        {
+            byte[] hostnameData = Encoding.UTF8.GetBytes(hostname);
+            byte[] hash = Hasher.ComputeHash(hostnameData);
+            return Encoding.UTF8.GetString(hash);
+        }
+    }
+}
