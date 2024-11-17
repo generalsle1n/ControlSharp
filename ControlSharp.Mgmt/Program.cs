bbb5dee8fe04517e1e3892fadbeef9412f99275e
Niels Schuler <niels.schuler@web.de>
2024-11-17 12:56:20 +0100

    Added Variable

--- ControlSharp.Mgmt/Program.cs
+++ ControlSharp.Mgmt/Program.cs
@@ -6,7 +6,7 @@ IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(ar
 IResourceBuilder<ProjectResource> ControlApi = builder.AddProject<ControlSharp_Api>("ControlSharp-Api")
     .WithReplicas(builder.Configuration.GetValue<int>("Replicas"));
 
-builder.AddProject<ControlSharp_Ui>("ControlSharp-Ui")
+IResourceBuilder<ProjectResource> ControlUi = builder.AddProject<ControlSharp_Ui>("ControlSharp-Ui")
     .WithReplicas(builder.Configuration.GetValue<int>("Replicas"))
     .WithReference(ControlApi)
     .WaitFor(ControlApi);
