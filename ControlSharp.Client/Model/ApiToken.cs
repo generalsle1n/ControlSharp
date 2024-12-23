c1a036afeded1f5c0e1c3aa4deeb437c40cd9934
Niels Schuler <niels.schuler@web.de>
2024-12-23 18:44:07 +0100

    Added Models

---
+++ ControlSharp.Client/Model/ApiToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ControlSharp.Client.Model
+{
+    public class ApiToken
+    {
+        [JsonPropertyName("tokenType")]
+        public string TokenType { get; set; }
+        [JsonPropertyName("accessToken")]
+        public string AccessToken { get; set; }
+        [JsonPropertyName("expiresIn")]
+        public int ExpiresIn { get; set; }
+        [JsonPropertyName("refreshToken")]
+        public string RefreshToken { get; set; }
+    }
+}
