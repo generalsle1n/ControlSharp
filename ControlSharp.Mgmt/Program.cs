45b6c8432f45add15c350338722a4f69afef1117
Niels Schuler <niels.schuler@web.de>
2025-01-07 17:29:20 +0100

    Added Idendity Project

--- ControlSharp.Mgmt/Program.cs
+++ ControlSharp.Mgmt/Program.cs
@@ -3,17 +3,26 @@ using Projects;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-IResourceBuilder<ProjectResource> ControlApi = builder.AddProject<ControlSharp_Api>("ControlSharp-Api")
+IResourceBuilder<ProjectResource> ControlIdentity = builder.AddProject<ControlSharp_Identity>("ControlSharp-Identity")
     .WithReplicas(builder.Configuration.GetValue<int>("Replicas"));
+
+IResourceBuilder<ProjectResource> ControlApi = builder.AddProject<ControlSharp_Api>("ControlSharp-Api")
+    .WithReplicas(builder.Configuration.GetValue<int>("Replicas"))
+    .WithReference(ControlIdentity)
+    .WaitFor(ControlIdentity);
 
 IResourceBuilder<ProjectResource> ControlUi = builder.AddProject<ControlSharp_Ui>("ControlSharp-Ui")
     .WithReplicas(builder.Configuration.GetValue<int>("Replicas"))
     .WithReference(ControlApi)
-    .WaitFor(ControlApi);
+    .WithReference(ControlIdentity)
+    .WaitFor(ControlApi)
+    .WaitFor(ControlIdentity);
 
 builder.AddProject<ControlSharp_Client>("ControlSharp-Client")
     .WaitFor(ControlUi)
     .WithEnvironment("server", ControlApi.GetEndpoint("https"));
+
+
 
 DistributedApplication app = builder.Build();
 await app.RunAsync();
