196bddff24e6d910d64deac47337e22866e7fe0f
Niels Schuler <niels.schuler@web.de>
2025-01-09 14:33:58 +0100

    Remove DB Creation

--- ControlSharp.Ui/Program.cs
+++ ControlSharp.Ui/Program.cs
@@ -6,7 +6,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.AddDatabase();
+//builder.AddDatabase();
+
 //Aspire
 builder.AddServiceDefaults();
 
