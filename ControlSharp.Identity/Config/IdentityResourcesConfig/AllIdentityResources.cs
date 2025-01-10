2db80b0bd44cf31f745d75c9568220f7936dc4c4
Niels Schuler <niels.schuler@web.de>
2025-01-10 20:21:32 +0100

    Added RoleIndentity Resourc

--- ControlSharp.Identity/Config/IdentityResourcesConfig/AllIdentityResources.cs
+++ ControlSharp.Identity/Config/IdentityResourcesConfig/AllIdentityResources.cs
@@ -1,4 +1,5 @@
 ﻿using Duende.IdentityServer.Models;
+using IdentityModel;
 
 namespace ControlSharp.Identity.Config.IdentityResourcesConfig
 {
@@ -9,6 +10,15 @@ namespace ControlSharp.Identity.Config.IdentityResourcesConfig
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
             new IdentityResources.Address(),
+            new IdentityResource()
+            {
+                Name = "roles",
+                DisplayName = "Object Roles",
+                UserClaims = new List<string>
+                {
+                    JwtClaimTypes.Role
+                }
+            }
         };
     }
 }
