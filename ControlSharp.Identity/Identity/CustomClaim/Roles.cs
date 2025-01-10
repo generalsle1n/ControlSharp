acf8be5745baa36da13d1a77c1653d7be26a2905
Niels Schuler <niels.schuler@web.de>
2025-01-10 20:21:53 +0100

    Added Roles

---
+++ ControlSharp.Identity/Identity/CustomClaim/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace ControlSharp.Identity.Identity.CustomClaim
+{
+    public enum Roles
+    {
+        ControlSharpApi_Asset_Read,
+        ControlSharpApi_Asset_Write,
+        ControlSharpApi_Super
+    }
+}
