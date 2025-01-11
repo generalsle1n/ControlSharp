899ef7a24a922ff996cdac247d4d8ed98664ddff
Niels Schuler <niels.schuler@web.de>
2025-01-11 23:15:44 +0100

    Added Raw Api Client

---
+++ ControlSharp.Identity/Config/Clients/ControlSharpApiClient.cs
@@ -0,0 +1,21 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace ControlSharp.Identity.Config.Clients
+{
+    internal class ControlSharpApiClient
+    {
+        private const string _name = "ControlSharp-Api";
+        internal static Client Client = new Client()
+        {
+            ClientName = _name,
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            RequirePkce = true,
+            AccessTokenType = AccessTokenType.Reference,
+            AllowedScopes = new List<string>()
+            {
+                //"ControlSharp-Api"
+            }
+        };
+    }
+}
