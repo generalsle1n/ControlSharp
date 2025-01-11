17acba6754dfc1193cb5f735a2c326a803c70ba7
Niels Schuler <niels.schuler@web.de>
2025-01-11 12:08:50 +0100

    Removed SignInManager

--- ControlSharp.Ui/Controllers/Asset/AssetController.cs
+++ ControlSharp.Ui/Controllers/Asset/AssetController.cs
@@ -16,12 +16,11 @@ public class AssetController : Controller
 {
     private readonly ILogger<AssetController> _logger;
     private IHttpClientFactory _httpClientFactory;
-    private SignInManager<User> _signInManager;
+
     public AssetController(ILogger<AssetController> logger, IHttpClientFactory httpClientFactory, SignInManager<User> signInManager)
     {
         _logger = logger;
         _httpClientFactory = httpClientFactory;
-        _signInManager = signInManager;
     }
     
     public async Task<IActionResult> Registered()
