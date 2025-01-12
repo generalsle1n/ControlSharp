cc96dd9f5a5726000822126f544bdd2c74a77be8
Niels Schuler <niels.schuler@web.de>
2025-01-12 11:28:36 +0100

    Adjutsed Assetcontroller for usage with access token

--- ControlSharp.Ui/Controllers/Asset/AssetController.cs
+++ ControlSharp.Ui/Controllers/Asset/AssetController.cs
@@ -12,68 +12,138 @@ public class AssetController : Controller
 {
     private readonly ILogger<AssetController> _logger;
     private IHttpClientFactory _httpClientFactory;
+    private const string _apiServerHostName = "ControlSharp-Api";
 
-    public AssetController(ILogger<AssetController> logger, IHttpClientFactory httpClientFactory, SignInManager<User> signInManager)
+    public AssetController(ILogger<AssetController> logger, IHttpClientFactory httpClientFactory/*, SignInManager<User> signInManager*/)
     {
         _logger = logger;
         _httpClientFactory = httpClientFactory;
     }
-    
+
+    [Authorize(Policy = nameof(Roles.ControlSharpApi_Super_Read))]
     public async Task<IActionResult> Registered()
     {
         HttpClient Client = _httpClientFactory.CreateClient();
-        HttpRequestMessage Request = await HttpRequestHelper.CreateRequestMessageWithApiAuthAsync("https://ControlSharp-Api/api/v0.1/Asset/Registered", HttpMethod.Get, User.Identity.Name, _signInManager);
-        HttpResponseMessage Response = await Client.SendAsync(Request);
-        
-        List<Database.Identity.Model.Asset> Result = null;
-        if (Response.IsSuccessStatusCode)
+
+        HttpRequestMessage RequestMessage = await CreateAuthorizedRequestMessage();
+        RequestMessage.Method = HttpMethod.Get;
+        RequestMessage.RequestUri = new Uri($"https://{_apiServerHostName}/api/v0.1/Asset/Registered");
+       
+        HttpResponseMessage ResponseMessage = await Client.SendAsync(RequestMessage);
+
+        List<ModelAsset.Asset> Result = new List<ModelAsset.Asset>();
+
+        if (ResponseMessage.IsSuccessStatusCode)
         {
-            Result = await Response.Content.ReadFromJsonAsync<List<Database.Identity.Model.Asset>>();
+            List<ModelAsset.Asset> Data = await ResponseMessage.Content.ReadFromJsonAsync<List<ModelAsset.Asset>>();
+            Result.AddRange(Data);
         }
-        
-        return View(Result);
-    }
-    public async Task<IActionResult> Quarantine()
-    {
-        HttpClient Client = _httpClientFactory.CreateClient();
-        HttpRequestMessage Request = await HttpRequestHelper.CreateRequestMessageWithApiAuthAsync("https://ControlSharp-Api/api/v0.1/Asset/Unregistered", HttpMethod.Get, User.Identity.Name, _signInManager);
-        HttpResponseMessage Response = await Client.SendAsync(Request);
-        
-        List<Database.Identity.Model.Asset> Result = null;
-        if (Response.IsSuccessStatusCode)
+        else
         {
-            Result = await Response.Content.ReadFromJsonAsync<List<Database.Identity.Model.Asset>>();
+            _logger.LogWarning($"{ResponseMessage.ReasonPhrase} To {ResponseMessage.RequestMessage.RequestUri.AbsolutePath}");
         }
-        
+
         return View(Result);
     }
 
-    public async Task<IActionResult> AddClient(Database.Identity.Model.Asset Asset)
+    [Authorize(Policy = nameof(Roles.ControlSharpApi_Super_Read))]
+    public async Task<IActionResult> Quarantine()
     {
         HttpClient Client = _httpClientFactory.CreateClient();
- 
-        HttpRequestMessage AssetRegisterRequest = await HttpRequestHelper.CreateRequestMessageWithApiAuthAsync(
-            $"https://ControlSharp-Api/api/v0.1/Asset/{Asset.Id}",
-            HttpMethod.Post,
-            User.Identity.Name,
-            _signInManager);
-        
-        HttpResponseMessage AssetRegisterResponse = await Client.SendAsync(AssetRegisterRequest);
-        
-        return RedirectToAction("Quarantine");
+
+        HttpRequestMessage RequestMessage = await CreateAuthorizedRequestMessage();
+        RequestMessage.Method = HttpMethod.Get;
+        RequestMessage.RequestUri = new Uri($"https://{_apiServerHostName}/api/v0.1/Asset/Unregistered");
+
+        HttpResponseMessage ResponseMessage = await Client.SendAsync(RequestMessage);
+
+        List<ModelAsset.Asset> Result = new List<ModelAsset.Asset>();
+
+        if (ResponseMessage.IsSuccessStatusCode)
+        {
+            List<ModelAsset.Asset> Data = await ResponseMessage.Content.ReadFromJsonAsync<List<ModelAsset.Asset>>();
+            IEnumerable<ModelAsset.Asset> NotBanned = Data.Where(asset => asset.Banned == false );
+            Result.AddRange(NotBanned);
+        }
+        else
+        {
+            _logger.LogWarning($"{ResponseMessage.ReasonPhrase} To {ResponseMessage.RequestMessage.RequestUri.AbsolutePath}");
+        }
+
+        return View(Result);
     }
-    public async Task<IActionResult> DeleteClient(Database.Identity.Model.Asset Asset)
+
+    [Authorize(Policy = nameof(Roles.ControlSharpApi_Super_Read))]
+    public async Task<IActionResult> Banned()
     {
         HttpClient Client = _httpClientFactory.CreateClient();
-        Client.Timeout = TimeSpan.FromMinutes(30);
-        HttpRequestMessage AssetRegisterRequest = await HttpRequestHelper.CreateRequestMessageWithApiAuthAsync(
-            $"https://ControlSharp-Api/api/v0.1/Asset/{Asset.Id}",
-            HttpMethod.Delete,
-            User.Identity.Name,
-            _signInManager);
-        
-        HttpResponseMessage AssetRegisterResponse = await Client.SendAsync(AssetRegisterRequest);
-        
-        return RedirectToAction("Quarantine");
+
+        HttpRequestMessage RequestMessage = await CreateAuthorizedRequestMessage();
+        RequestMessage.Method = HttpMethod.Get;
+        RequestMessage.RequestUri = new Uri($"https://{_apiServerHostName}/api/v0.1/Asset/Unregistered");
+
+        HttpResponseMessage ResponseMessage = await Client.SendAsync(RequestMessage);
+
+        List<ModelAsset.Asset> Result = new List<ModelAsset.Asset>();
+
+        if (ResponseMessage.IsSuccessStatusCode)
+        {
+            List<ModelAsset.Asset> Data = await ResponseMessage.Content.ReadFromJsonAsync<List<ModelAsset.Asset>>();
+            IEnumerable<ModelAsset.Asset> NotBanned = Data.Where(asset => asset.Banned == true);
+            Result.AddRange(NotBanned);
+        }
+        else
+        {
+            _logger.LogWarning($"{ResponseMessage.ReasonPhrase} To {ResponseMessage.RequestMessage.RequestUri.AbsolutePath}");
+        }
+
+        return View(Result);
+    }
+
+    public async Task<IActionResult> AddClient(ModelAsset.Asset Asset)
+    {
+
+        HttpClient Client = _httpClientFactory.CreateClient();
+
+        HttpRequestMessage RequestMessage = await CreateAuthorizedRequestMessage();
+        RequestMessage.Method = HttpMethod.Post;
+        RequestMessage.RequestUri = new Uri($"https://{_apiServerHostName}/api/v0.1/Asset/{Asset.Id}");
+
+        HttpResponseMessage AssetRegisterResponse = await Client.SendAsync(RequestMessage);
+
+        if(!AssetRegisterResponse.IsSuccessStatusCode)
+        {
+            _logger.LogWarning($"{AssetRegisterResponse.ReasonPhrase} To {AssetRegisterResponse.RequestMessage.RequestUri.AbsolutePath}");
+        }
+
+        return RedirectToAction("Registered");
+    }
+
+    public async Task<IActionResult> DeleteClient(ModelAsset.Asset Asset)
+    {
+        HttpClient Client = _httpClientFactory.CreateClient();
+
+        HttpRequestMessage RequestMessage = await CreateAuthorizedRequestMessage();
+        RequestMessage.Method = HttpMethod.Delete;
+        RequestMessage.RequestUri = new Uri($"https://{_apiServerHostName}/api/v0.1/Asset/{Asset.Id}");
+
+        HttpResponseMessage AssetDeleteResponse = await Client.SendAsync(RequestMessage);
+
+        if (!AssetDeleteResponse.IsSuccessStatusCode)
+        {
+            _logger.LogWarning($"{AssetDeleteResponse.ReasonPhrase} To {AssetDeleteResponse.RequestMessage.RequestUri.AbsolutePath}");
+        }
+
+        return RedirectToAction("Banned");
+    }
+
+    private async Task<HttpRequestMessage> CreateAuthorizedRequestMessage()
+    {
+        HttpRequestMessage Result = new HttpRequestMessage();
+
+        string AccessToken = await HttpContext.GetTokenAsync("access_token");
+        Result.Headers.Authorization = new AuthenticationHeaderValue("Bearer", AccessToken);
+
+        return Result;
     }
 }
